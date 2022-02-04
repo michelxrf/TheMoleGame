01f372c0b77206f21869d8ec9097a3e02be50d3d
Michel Fonseca <michelxrf@gmail.com>
2022-02-04 10:30:20 -0300

    fixed problems caused by changing unity's verison

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//////////////////////////////////
+// MapData dictionary
+// 0 = floor only
+// 1 = solid wall
+// 2 = breakable wall
+// 3 = level end
+//////////////////////////////////
+
+public class LevelGenerator : MonoBehaviour {
+
+	public GameObject floorPrefab;
+	public GameObject wallPrefab;
+
+	public GameObject characterController;
+
+	public GameObject floorParent;
+	public GameObject wallsParent;
+
+	public int mazeSize;
+
+	// 2D array representing the map
+	private int[,] mapData;
+
+	// we use these to dig through our maze and to spawn the pickup at the end
+	private int mazeX = 4, mazeY = 1;
+
+	// Use this for initialization
+	void Start () {
+
+		// initialize map 2D array
+		mapData = GenerateMazeData();
+
+		// create actual maze blocks from maze boolean data
+		for (int z = 0; z < mazeSize; z++) {
+			for (int x = 0; x < mazeSize; x++) {
+				if (mapData[z, x] == 1) { // solid walls
+					CreateChildPrefab(wallPrefab, wallsParent, x, 1, z);
+
+					// create floor below walls
+					CreateChildPrefab(floorPrefab, floorParent, x, 0, z);
+				} 
+                else if(mapData[z, x] == 0){ // floor only
+					CreateChildPrefab(floorPrefab, floorParent, x, 0, z);
+				}
+                // TODO: Generate all the other types of tiles
+                // elseif(isso){bota isso};
+
+			}
+		}
+	}
+
+	// generates the booleans determining the maze, which will be used to construct the cubes
+	// actually making up the maze
+	int[,] GenerateMazeData() {
+		int[,] data = new int[mazeSize, mazeSize];
+
+		// TODO: procgen the level map
+        for(int z = 0; z < mazeSize; z++)
+        {
+            for (int x = 0; x < mazeSize; x++)
+            {
+                if(x == 0 || x == mazeSize - 1 || z == 0 || z == mazeSize - 1) // map border
+                {
+                    data[x,z] = 1;
+                }
+                else
+                {
+                    data[x,z] = 0;
+                }
+            }
+        }
+
+		return data;
+	}
+
+	// allow us to instantiate something and immediately make it the child of this game object's
+	// transform, so we can containerize everything. also allows us to avoid writing Quaternion.
+	// identity all over the place, since we never spawn anything with rotation
+	void CreateChildPrefab(GameObject prefab, GameObject parent, int x, int y, int z) {
+		var myPrefab = Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity);
+		myPrefab.transform.parent = parent.transform;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform camera;
+
+    public float speed = 1f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    private void Start()
+    {
+        Cursor.visible = false;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude > 0)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, camera.eulerAngles.y, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle , 0f);
+            
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime );
+        }
+    }
+}
