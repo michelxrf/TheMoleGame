a00edc8e2da288760436d3e28f52a50661783a52
Michel Fonseca <michelxrf@gmail.com>
2022-02-07 18:50:05 -0300

    created breakable walls and fixed a handfull of bugs

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -15,37 +15,46 @@ public class LevelGenerator : MonoBehaviour {
 
 	public GameObject floorPrefab;
 	public GameObject wallPrefab;
+	public GameObject breakablePrefab;
 
 	public GameObject characterController;
 
 	public GameObject floorParent;
 	public GameObject wallsParent;
 
-	public int mazeSize;
+	public int mazeSizeX;
+	public int mazeSizeZ;
+	public float percentageToClear;
 
 	// 2D array representing the map
 	private int[,] mapData;
 
-	// we use these to dig through our maze and to spawn the pickup at the end
-	private int mazeX = 4, mazeY = 1;
-
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
 		// initialize map 2D array
 		mapData = GenerateMazeData();
 
 		// create actual maze blocks from maze boolean data
-		for (int z = 0; z < mazeSize; z++) {
-			for (int x = 0; x < mazeSize; x++) {
-				if (mapData[z, x] == 1) { // solid walls
-					CreateChildPrefab(wallPrefab, wallsParent, x, 1, z);
+		for (int z = 0; z < mazeSizeZ; z++) {
+			for (int x = 0; x < mazeSizeX; x++) {
+				if (mapData[x, z] == 1) { // solid walls
+					CreateChildPrefab(wallPrefab, wallsParent, x, 1, z, 7);
 
 					// create floor below walls
-					CreateChildPrefab(floorPrefab, floorParent, x, 0, z);
+					CreateChildPrefab(floorPrefab, floorParent, x, 0, z, 7);
 				} 
-                else if(mapData[z, x] == 0){ // floor only
-					CreateChildPrefab(floorPrefab, floorParent, x, 0, z);
+				else if(mapData[x, z] == 2) //breakable block
+				{ 
+					CreateChildPrefab(breakablePrefab, wallsParent, x, 1, z, 8);
+
+					// create floor below walls
+					CreateChildPrefab(floorPrefab, floorParent, x, 0, z, 7);
+				}
+                else if(mapData[x, z] == 0)// floor only
+				{ 
+					CreateChildPrefab(floorPrefab, floorParent, x, 0, z, 7);
 				}
                 // TODO: Generate all the other types of tiles
                 // elseif(isso){bota isso};
@@ -56,24 +65,80 @@ public class LevelGenerator : MonoBehaviour {
 
 	// generates the booleans determining the maze, which will be used to construct the cubes
 	// actually making up the maze
-	int[,] GenerateMazeData() {
-		int[,] data = new int[mazeSize, mazeSize];
+	int[,] GenerateMazeData()
+	{
+		int[,] data = new int[mazeSizeX, mazeSizeZ];
 
-		// TODO: procgen the level map
-        for(int z = 0; z < mazeSize; z++)
+		// Generate base map
+        for(int z = 0; z < mazeSizeZ; z++)
         {
-            for (int x = 0; x < mazeSize; x++)
+            for (int x = 0; x < mazeSizeX; x++)
             {
-                if(x == 0 || x == mazeSize - 1 || z == 0 || z == mazeSize - 1) // map border
+                if(x == 0 || x == mazeSizeX - 1 || z == 0 || z == mazeSizeZ - 1) // map border
                 {
                     data[x,z] = 1;
                 }
                 else
                 {
-                    data[x,z] = 0;
+                    data[x,z] = 2;
                 }
             }
         }
+
+		// Generate rooms
+		//need to think if its better to keep rounding the number as I go or keep calculating with
+		//real numbers and only round it once I record the room dimensions
+		//they both will work the same way, but one way will be more efficient
+
+		//calculate the inner area between the outer walls
+		int innerTilesAmount = mazeSizeX * mazeSizeZ - 2 * (mazeSizeZ + mazeSizeX);
+		//calculate the amount of tiles that will be removed form the room
+		float tilesToRemove = innerTilesAmount * percentageToClear;
+
+		//calculate the amount of rooms are going to be made
+		int numberOfRooms = Random.Range(3, 9);
+
+		//calculate the base size for the rooms
+		float roomBaseSize = tilesToRemove / numberOfRooms;
+
+		for(int room = 0; room < numberOfRooms; room++)
+		{
+			////////////////////////////////////////////////////////////
+			//calculate a random size variation for each room
+			//define coordinates to make the room
+			//get square root of roomSize
+			//multiple the squareRoot by a randomRange (from 0 to 1) to get nonsquare rooms sometimes
+			//one of the sides gets the squareRoot*Range and the other gets squareRoot*(1 - Range)
+			//this gives me the room dimensions to an aproximate of the calculated area
+			//then pick a random starting position to start drawing the room
+			//the starting point takes in consideration its max value to respect maze size
+			
+			float thisRoomSize = Random.Range(1f, 3f) * roomBaseSize;
+
+			float rootedDimensions = Mathf.Sqrt(thisRoomSize);
+			float roomFormatFactor = Random.value;
+
+			int roomDimensionX = Mathf.FloorToInt(rootedDimensions * roomFormatFactor);
+			int roomDimensionZ = Mathf.FloorToInt(rootedDimensions * (1 - roomFormatFactor));
+
+			int startingPointX = Random.Range(1, mazeSizeX - roomDimensionX);
+			int startingPointZ = Random.Range(1, mazeSizeZ - roomDimensionZ);
+
+			for(int z = startingPointZ; z < startingPointZ + roomDimensionZ; z++)
+			{
+				for(int x = startingPointX; x < startingPointX + roomDimensionX; x++)
+				{
+					if(x != 0 && x != mazeSizeX-1 && z != 0 && z != mazeSizeZ-1)
+					{
+						data[x,z] = 0;
+					}
+					else
+					{
+						Debug.Log("Room is too big!");
+					}
+				}
+			}
+		}
 
 		return data;
 	}
@@ -81,8 +146,10 @@ public class LevelGenerator : MonoBehaviour {
 	// allow us to instantiate something and immediately make it the child of this game object's
 	// transform, so we can containerize everything. also allows us to avoid writing Quaternion.
 	// identity all over the place, since we never spawn anything with rotation
-	void CreateChildPrefab(GameObject prefab, GameObject parent, int x, int y, int z) {
+	void CreateChildPrefab(GameObject prefab, GameObject parent, int x, int y, int z, int objectLayer)
+	{
 		var myPrefab = Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity);
+		myPrefab.layer = objectLayer;
 		myPrefab.transform.parent = parent.transform;
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,12 @@ public class PlayerController : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask monstersLayer;
+    public LayerMask destructiblesLayer;
+    public LayerMask undestructiblesLayer;
+
     private void Start()
     {
         Cursor.visible = false;    
@@ -19,6 +25,11 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+        Strike();
+    }
+    void Move()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
@@ -33,6 +44,32 @@ public class PlayerController : MonoBehaviour
             
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * speed * Time.deltaTime );
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = new Vector3(transform.position.x, .4f, transform.position.z);
+        }
+    }
+
+    void Strike()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            //TODO: play animation
+            
+            //TODO: detect enemies
+            //TODO: detect destructibles
+            Collider[] hitWalls = Physics.OverlapSphere(attackPoint.position, attackRange, destructiblesLayer);
+            //TODO: detect undestructibles
+
+            //TODO: hurt hit enemies
+            foreach (Collider breakableWall in hitWalls)
+            {
+                Destroy(breakableWall.transform.gameObject);      
+            }
+            //TODO: don't destroy walls
+        
+        
         }
     }
 }
